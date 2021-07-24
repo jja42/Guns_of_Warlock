10de6e915d656ec62416e2614d902cd402c9e305
Andrew Dinspechin <58988010+akvaxthelich@users.noreply.github.com>
2021-07-24 17:43:23 -0400

    Ink? also camera fixes

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,17 +4,19 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour {
     public float rightLimit;
+    public float leftLimit;
     GameObject player;
 
     void Start() {
         player = GameObject.FindGameObjectWithTag("Player");
+        transform.position = new Vector3(leftLimit, 0, -10);
     }
 
     void Update() {
         var playerPos = player.transform.position;
         var cameraPos = transform.position;
 
-        if (playerPos.x > 0 && playerPos.x < rightLimit) {
+        if (playerPos.x > leftLimit && playerPos.x < rightLimit) {
             cameraPos.x = playerPos.x;
             transform.position = cameraPos;
         }
