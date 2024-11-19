35b1b6d430c45db3efbc99a02ed46a49c0f2225c
jja42 <jja42w@gmail.com>
2024-11-19 13:23:38 -0500

    Normalized Movement Input Vector

--- Assets/Scripts/Game_Manager.cs
+++ Assets/Scripts/Game_Manager.cs
@@ -144,6 +144,7 @@ public class Game_Manager : MonoBehaviour
     void OnMove(InputValue value)
     {
         Vector2 inputvector = value.Get<Vector2>();
+        inputvector = inputvector.normalized;
         player.PlayerMove(inputvector);
     }
 
